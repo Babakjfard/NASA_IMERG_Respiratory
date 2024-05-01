b534c746086833d4c678149b262241462d3cf7df
Babakjfard <babak.jfard@gmail.com>
2024-04-30 20:44:51 -0400

    ACS 5-year data

--- src/ACS_data.R
+++ src/ACS_data.R
@@ -4,74 +4,67 @@ library(dplyr)
 library(purrr)
 
 # Set your API key
-census_api_key("your_key_here")  # replace 'your_key_here' with your actual API key
+# census_api_key("8ad6189aea3b7a7a8dc17a87aa65c8559e10ef07", install = TRUE, overwrite = TRUE)  # replace 'your_key_here' with your actual API key
 
 # Variables of interest with ACS code
+setwd('/Users/babak.jfard/projects/NASA_IMERG_Respiratory/')
 
 vars <- c( # For 2011 -2019
-  Poverty_Total = "B17001_001", Poverty_below = "B17001_002", 
+  "B17001_001", "B17001_002", # Poverty: Total, Below poverty line
   
-  FedAssist = "B19058_002", FedAssist_Total = "B19058_001",
+  "B99221_001", "B99221_002",      # Imputed Food Stamp: Total, Imputed
   
-  NoHighSchool = c(paste0("B15003_00", 2:9), paste0("B15003_0", 10:16)),
-  Education_total = "B15003_001",
-  
-  # Health Coverage
-  Total_Pop_Insurance = "B27001_001",
-  Not_Insured = c(paste0("B27001_00", seq(5, 8, 3)),
-                    paste0("B27001_0", seq(11, 29, 3), "E"),
-                    paste0("B27001_0", seq(33, 57,3), "E")),
-  
-  Unemployment_civilian = "B23025_005", #Total! In labor Force! Civilian labor force! Unemployed!
-  labor_force_civilian = "B23025_003"
+  c(paste0("B15002_00", 3:9), paste0("B15002_010")), #Below high school, Male
+  "B15002_002",     # Educational attainment over 25: Male Total 
+  c(paste0("B15002_0", 20:27)), #Below high school, Female
+  "B15002_019"    # Educational attainment over 25: FeMale Total 
 )
 
-vars <- c( # For 2005 -2010
-  Poverty_Total = "B17001_001", Poverty_below = "B17001_002", 
-  
-  FedAssist = "B19058_002", FedAssist_Total = "B19058_001",
-  
-  NoHighSchool = c(paste0("B15003_00", 2:9), paste0("B15003_0", 10:16)),
-  Education_total = "B15003_001",
-  
-  # Health Coverage
-  Total_Pop_Insurance = "B27001_001",
-  Not_Insured = c(paste0("B27001_00", seq(5, 8, 3)),
-                  paste0("B27001_0", seq(11, 29, 3), "E"),
-                  paste0("B27001_0", seq(33, 57,3), "E")),
-  
-  Unemployment_civilian = "B18120_012",   # Estimate!!Total!!In the labor force!!Unemployed
-  labor_force_civilian = "B18120_002"     # Estimate!!Total!!In the labor force
-)
 
 # Years to retrieve data for (ACS 1-year data available from 2005 onwards)
-years <- list(2)
-years[[1]] <- 2005:2010
-years[[2]] <- 2011:2019
-
+years <- 2009:2021
 # Function to get ACS data for a given year
-get_acs_data_for_year <- function(year) {
-  get_acs(geography = "county",
-          variables = vars,
-          state = "CA",
-          survey = "acs1",
-          year = year) %>%
-    mutate(year = year)  # Add year column to identify the data year
+states = c('CA', 'GA', 'OR', 'SC')
+
+for (state in states){
+
+  get_acs_data_for_year <- function(year) {
+    get_acs(geography = "county",
+            variables = vars,
+            state = state,
+            survey = "acs5",
+            year = year,
+            geometry = FALSE) %>%
+      mutate(year = year)  # Add year column to identify the data year
+  }
+  
+  # Retrieve data for all years using map_df to combine into a single data frame
+  all_years_data <- map_df(years, get_acs_data_for_year)
+  
+  print(paste(">>>>>>>>>>> The State is:", state))
+  print(head(all_years_data))
+  print(colSums(is.na(all_years_data)))
+  print(length(unique(all_years_data$GEOID)))
+  
+  # Check if some statistics to be consistent for all years
+  print(all_years_data %>% group_by(year) %>%
+    summarise(Total_rows = n(), counties=length(unique(GEOID)),
+              vars= length(unique(variable))))
+  
+  # Cleaning and spreading the data for readability
+  data_cleaned <- all_years_data %>%
+    select(GEOID, variable, estimate, year) %>%
+    pivot_wider(names_from = variable, values_from = estimate)
+  
+  final_acs <- data_cleaned %>% mutate(Poverty = B17001_002/B17001_001,
+                                       Food_STAMP = B99221_002/B99221_001,
+                                       Education = (rowSums(across(B15002_003:B15002_010))+
+                                                      rowSums(across(B15002_020:B15002_027)))/
+                                                      (B15002_002+B15002_019)) %>%
+    select(c(Poverty, Food_STAMP, Education))
+  
+  colSums(is.na(final_acs))
+  write_csv(final_acs, paste0('Data/interim/', state,"_acs5.csv"))
 }
-
-# Retrieve data for all years using map_df to combine into a single data frame
-all_years_data <- map_df(years[[1]], get_acs_data_for_year)
-
-head(all_years_data)
-colSums(is.na(all_years_data))
-length(unique(all_years_data$GEOID))
-
-# Cleaning and spreading the data for readability
-data_cleaned <- all_years_data %>%
-  select(NAME, variable, estimate, moe, year) %>%
-  spread(key = variable, value = estimate)
-
-# View the cleaned data
-print(data_cleaned)
 
 
